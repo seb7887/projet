72f57360c5c285c362835995478cf92f573cfce1
Seba <ssegura@inti.gob.ar>
2018-09-06 16:05:13 -0300

    api: auth middleware added

---
+++ api/middleware/auth.js
@@ -0,0 +1,36 @@
+const jwt = require('jsonwebtoken');
+
+const requireAuth = (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(' ')[1];
+  jwt.verify(token, process.env.SECRET_KEY || 'jwtsecret', (err, decoded) => {
+    if (decoded) {
+      next();
+    } else {
+      return next({ status: 401, message: 'Please log in first' });
+    }
+  });
+  } catch (e) {
+    return next({ status: 401, message: 'Please log in first' });
+  }
+}
+
+const checkProjectOwner = (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(' ')[1];
+    jwt.verify(token, process.env.SECRET_KEY || 'jwtsecret', (err, decoded) => {
+      if (decoded && decoded.id === req.params.id) {
+        return next();
+      } else {
+        return next({ status: 401, message: 'Unauthorized' });
+      }
+    })
+  } catch (e) {
+    return next({ status: 401, message: 'Unauthorized' });
+  }
+}
+
+module.exports = {
+  requireAuth: requireAuth,
+  checkProjectOwner: checkProjectOwner,
+}
--- api/routes/project.js
+++ api/routes/project.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router({ mergeParams: true });
 
 const project = require('../controllers/project');
+const auth = require('../middleware/auth');
 
 // prefix - /api/projects
-router.route('/').get(project.getProjects);
-router.route('/').post(project.createProject);
-router.route('/:project_id').delete(project.deleteProject);
+router.route('/').get(auth.requireAuth, project.getProjects);
+router.route('/').post(auth.requireAuth, project.createProject);
+router.route('/:project_id').delete(auth.requireAuth, auth.checkProjectOwner, project.deleteProject);
 
 module.exports = router;
